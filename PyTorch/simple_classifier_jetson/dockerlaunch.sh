eda0cb74f22fa4759e981ada045ac16e78a8a94f
Giarra, Matthew N <matthew.giarra@jhuapl.edu>
2020-06-30 12:56:43 -0400

    Update dockerlaunch.sh

--- PyTorch/simple_classifier_jetson/dockerlaunch.sh
+++ PyTorch/simple_classifier_jetson/dockerlaunch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --runtime nvidia --network host -v $(pwd):/workspace testimage
+docker run -it --rm --runtime nvidia --network host -v $(pwd):/workspace simple_classifier_jetson
