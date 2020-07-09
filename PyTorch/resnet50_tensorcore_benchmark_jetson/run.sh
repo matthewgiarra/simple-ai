27696dfc9fb9b4790f38597a7be16bc100f9c04a
Matthew Giarra <matthew.giarra@jhuapl.edu>
2020-07-09 17:17:24 -0400

    Update run.sh and Dockerfile in resnet50_tensorcore_benchmark_jetson.py

--- PyTorch/resnet50_tensorcore_benchmark_jetson/run.sh
+++ PyTorch/resnet50_tensorcore_benchmark_jetson/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker run -it --rm --runtime nvidia --network host -v $(pwd):/workspace resnet50_tensorcore_bechmark_jetson python3 resnet50_tensorcore_bechmark_jetson.py
 
+# Uncomment below (and comment above) to run on normal machine (i.e., not a Jetson)
+# docker run -it --rm --gpus all --network host --ipc=host -v $(pwd):/workspace resnet50_tensorcore_bechmark_jetson python3 resnet50_tensorcore_bechmark_jetson.py
